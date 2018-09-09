0137b1ff5bb41badf3940c6d17e3fc1b75c98d15
Danny Lin <danny0838@gmail.com>
2018-09-09 15:27:31 +0800

    test: remove suspend tab (for possible conflict with ublock origin?)

--- src/background.js
+++ src/background.js
@@ -1,6 +1,5 @@
 let filter;
 let updateFilterPromise;
-let suspendedTabs = new Map();
 
 let _isFxBelow56;
 Promise.resolve().then(() => {
@@ -119,19 +118,8 @@ function updateContextMenus() {
   });
 }
 
-// ref: https://github.com/gorhill/uBlock/issues/2067
-// Suspend all tabs until updateFilter is completed, and then this function
-// will be replaced and the suspended tabs will be loaded.
-//
-// @TODO:
-// This could still fail if the browser loads tabs before the extensions are loaded
-// (Chrome and Firefox for Android seems so).
-// In this case, we fallback to block on content script starting.
-let onBeforeRequestCallback = function (details) {
-  const {tabId, url} = details;
-  suspendedTabs.set(tabId, url);
-  return {cancel: true};
-};
+// dummy function.  Will be replaced after updateFilter() completed.
+let onBeforeRequestCallback = function (details) {};
 
 chrome.webRequest.onBeforeRequest.addListener((details) => {
   return onBeforeRequestCallback(details);
@@ -285,9 +273,4 @@ updateFilter().then(() => {
       return {redirectUrl: redirectUrl};
     }
   };
-
-  // load the suspended tabs
-  suspendedTabs.forEach((url, tabId) => {
-    chrome.tabs.update(tabId, {url: url, active: false});
-  });
 });
